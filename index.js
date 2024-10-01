9798b6d9921706ac7a87b4a809ef9dd3537254be
marvicweb3 <marvicweb3@gmail.com>
2024-10-01 05:46:06 +0300

    Update index.js

--- index.js
+++ index.js
@@ -11,14 +11,14 @@ const port = process.env.PORT || 4040;
 const hook = process.env.WEBHOOK_URL;
 const { BOT_TOKEN, SERVER_URL } = process.env;
 
-const TELEGRAM_API = `https://api.telegram.org/bot${BOT_TOKEN}`;
-const URI = `/webhook/${BOT_TOKEN}`;
+const TELEGRAM_API = https://api.telegram.org/bot${BOT_TOKEN};
+const URI = /webhook/${BOT_TOKEN};
 const WEBHOOK_URL = SERVER_URL + URI;
 
 app.use(express.json());
 app.use(bodyParser.json());
 const init = async () => {
-    const res = await axios.get(`${TELEGRAM_API}/setWebhook?url=${WEBHOOK_URL}`);
+    const res = await axios.get(${TELEGRAM_API}/setWebhook?url=${WEBHOOK_URL});
     console.log(res.data);
 };
 
@@ -29,26 +29,28 @@ app.listen(process.env.PORT || 5000, async () => {
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
 
-const web_link = "https://vanillaswap.netlify.app/";
+const web_link = "https://godzillaonton.netlify.app/";
 
 bot.start((ctx) => {
     const startPayload = ctx.startPayload;
-    const urlSent = `${web_link}?ref=${startPayload}`;
-    // const urlSentTwo = `${web_linkTwo}?ref=${startPayload}`;
+    const urlSent = ${web_link}?ref=${startPayload};
     const user = ctx.message.from;
-    const userName = user.username ? `@${user.username}` : user.first_name;
-    ctx.replyWithMarkdown(`*Hey, ${userName}! Welcome to Demo Tap App!*
-
-Click on Play Now to start mining Ton Coin`, {
+    const userName = user.username ? @${user.username} : user.first_name;
+    ctx.replyWithMarkdown(*Hey, ${userName}! Welcome to $GODZ Tap App!*, {
         reply_markup: {
             inline_keyboard: [
-                [{ text: "⚡️Play now!⚡️", web_app: { url: urlSent } }],
-                [{ text: "🧩 Join Our Telegram Channel 🧩", url: "https://t.me/demotest101" }]
-                // [{ text: "Bot App Demo 2 🧩", web_app: { url: urlSentTwo } }],
+                [{ text: "Play now!", web_app: { url: urlSent } }],
+                [{ text: " Join Our Telegram Channel ", url: "https://t.me/gozillaontonportal" }]
             ],
             in: true
         },
     });
+});
+
+// Command to send a picture
+bot.command('sendphoto', (ctx) => {
+    const photoUrl = 'https://i.postimg.cc/y6kn1gCV/Untitled-design-7.png'; // Replace with your image URL
+    ctx.replyWithPhoto(photoUrl, { caption: 'Here is your photo!' });
 });
 
 app.get("/", async (req, res) => {
